fe260cd22f58572b00c2e66e509949460b8b35ec
Patrick Carvalho Malta <patrick.malta@dcoimbra.com.br>
2026-04-30 17:14:38 -0400

    starting new structure

--- internal/model/weather.go
+++ internal/model/weather.go
@@ -2,10 +2,18 @@ package model
 
 import "time"
 
+type StationInfo struct {
+	ID          string `json:"id"`
+	StationName string `json:"stationName"`
+}
+
 type WeatherRequest struct {
-	Pressure uint32 `json:"pressure" binding:"required"`
-	Humidity uint16 `json:"humidity" binding:"required"`
-	Temp     int16  `json:"temp" binding:"required"`
+	StationID string  `json:"station_id"`
+	Pressure  uint32  `json:"pressure" binding:"required"`
+	Humidity  uint16  `json:"humidity" binding:"required"`
+	Temp      int16   `json:"temp" binding:"required"`
+	Lat       float64 `json:"lat" binding:"required"`
+	Long      float64 `json:"long" binding:"required"`
 }
 
 type WeatherResponse struct {
@@ -13,5 +21,7 @@ type WeatherResponse struct {
 	Pressure  uint32    `json:"pressure"`
 	Humidity  uint16    `json:"humidity"`
 	Temp      int16     `json:"temp"`
+	Lat       float64   `json:"lat"`
+	Long      float64   `json:"long"`
 	CreatedAt time.Time `json:"created_at"`
 }
